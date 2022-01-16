78163edb4c32c11e7ea47f152e712d75c736aa3f
Eliot Hertenstein <39346975+eIiot@users.noreply.github.com>
2022-01-15 22:25:04 -0800

    move over more repos, steal tree from /scripts

---
+++ index.js
@@ -0,0 +1,25 @@
+fetch('https://api.github.com/repos/eiiot/math/contents')
+  .then(async function(response) {
+    const data = await response.json();
+    const div = document.querySelector('#content');
+
+    div.innerHTML = '';
+
+    for (let i = 0; i < data.length; i++) {
+      const file = data[i];
+
+      if (file.type == 'dir' && !file.name.startsWith('.')) {
+        // check if folder contains index.html
+        const index = await fetch(`https://api.github.com/repos/eIiot/math/contents/${file.name}/index.html`)
+        // handle error
+        if (!index.ok) {continue};
+        console.log(index.ok);
+        var a = document.createElement('a');
+        a.classList.add('text-cyan-600');
+        a.href = './' + file.name;
+        a.innerHTML = file.name;
+        div.appendChild(a);
+        div.appendChild(document.createElement('br'));
+      };
+    };
+  });
